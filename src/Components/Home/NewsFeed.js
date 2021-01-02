5c80afcf9be3ff1b3857c1e4bdbf346324b4f847
saifulshihab <shihabmd1970@gmail.com>
2021-01-02 12:28:41 +0600

    fix myDaylist & added post_form

--- src/Components/Home/NewsFeed.js
+++ src/Components/Home/NewsFeed.js
@@ -1,43 +1,62 @@
 import React from 'react';
+import { myDay } from '../../DemoData';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import PhotoIcon from '@material-ui/icons/Photo';
+import MoodIcon from '@material-ui/icons/Mood';
 
 function Newsfeed() {
+  console.log(myDay);
   return (
     <>
-      <div id="newsfeed">
-        <div className="mydayList">
+      <div id='newsfeed'>
+        <div className='mydayList'>
           <ul>
-            <li>
-              <div className="myday">
-                <img src="#" />
-                <p>Saiful Shihab</p>
-              </div>
-            </li>
-            <li>
-              <div className="myday">
-                <img src="#" />
-                <p>Saiful Shihab</p>
-              </div>
-            </li>
-            <li>
-              <div className="myday">
-                <img src="#" />
-                <p>Saiful Shihab</p>
-              </div>
-            </li>
-            <li>
-              <div className="myday">
-                <div className="img">
-                  <img src="#" />
+            {myDay.map((data) => (
+              <li key={data.id}>
+                <div
+                  className='myday'
+                  style={{ backgroundImage: `url(${data.mydayimg})` }}
+                >
+                  <div className='mydaydp'>
+                    <img src={data.dp} />
+                  </div>
+                  <div className='myDayusername'>
+                    <p>{data.name}</p>
+                  </div>
                 </div>
-                <div className="p">
-                  <p>Saiful Shihab</p>
-                </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="post_form"></div>
-        <div className="posts"></div>
+        <div className='post_form'>
+          <div className='top_content'>
+            <div className='user_img'>
+              <img
+                src={require('../../assets/images/profile.JPG')}
+                alt='name'
+              />
+            </div>
+            <div className='post_btn'>
+              <button>What's on your mind? John</button>
+            </div>
+          </div>
+          <div className='middle_content'></div>
+          <div className='bottom_content'>
+            <button>
+              <VideoCallIcon style={{ color: 'red', marginRight: 4 }} /> Live
+              Video
+            </button>
+            <button>
+              <PhotoIcon style={{ color: '#00c14d', marginRight: 4 }} />{' '}
+              Photo/Video
+            </button>
+            <button>
+              <MoodIcon style={{ color: '#ffd400', marginRight: 4 }} />{' '}
+              Feeling/Activity
+            </button>
+          </div>
+        </div>
+        <div className='posts'></div>
       </div>
     </>
   );
---
+++ src/DemoData.js
@@ -0,0 +1,51 @@
+export const myDay = [
+  {
+    id: 1,
+    name: 'Saiful Islam',
+    dp: require('../src/assets/images/profile.JPG'),
+    mydayimg: require('../src/assets/images/profile.JPG'),
+  },
+  {
+    id: 2,
+    name: 'John Doe',
+    dp: require('../src/assets/images/profile.JPG'),
+    mydayimg: require('../src/assets/images/profile.JPG'),
+  },
+  {
+    id: 3,
+    name: 'Jane Hak',
+    dp: require('../src/assets/images/profile.JPG'),
+    mydayimg: require('../src/assets/images/profile.JPG'),
+  },
+  {
+    id: 4,
+    name: 'Hardin Kop',
+    dp: require('../src/assets/images/profile.JPG'),
+    mydayimg: require('../src/assets/images/profile.JPG'),
+  },
+];
+
+export const post = [
+  {
+    id: 1,
+    user_fname: 'Shihab',
+    dp: require('../src/assets/images/profile.JPG'),
+    createdAt: '2h',
+    caption: 'Hi',
+    image: require('../src/assets/images/world_end.jpg'),
+    like: 45,
+    comment: 5,
+    share: 2,
+  },
+  {
+    id: 2,
+    user_fname: 'John Doe',
+    dp: require('../src/assets/images/profile.JPG'),
+    createdAt: '2h',
+    caption: '',
+    image: require('../src/assets/images/world_end.jpg'),
+    like: 5,
+    comment: 1,
+    share: 1,
+  },
+];
