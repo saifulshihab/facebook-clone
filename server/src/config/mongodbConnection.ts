4af95111180a423429c1eb8486dfa98fab39d70c
saifulshihab <shihabmd1970@gmail.com>
2021-12-13 23:55:05 +0600

    chore: rename file

--- server/src/config/mongodbConnection.ts
+++ server/src/config/mongodbConnection.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import mongoose from 'mongoose';
 
-async function connectDB() {
+async function connectMongoDB() {
   try {
     const connection = await mongoose.connect(
       process.env.DB_URL || 'mongodb://localhost:27017/facebook-clone'
@@ -12,4 +12,4 @@ async function connectDB() {
   }
 }
 
-export { connectDB };
+export { connectMongoDB };
